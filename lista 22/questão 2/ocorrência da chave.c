8778a749c87177825cc0ceaab53d4f6999cd7e6f
Vicenzo <marcosvicenzo8@gmail.com>
2024-06-23 23:36:40 -0300

    more exercices made

--- lista 22/questão 2/ocorrência da chave.c
+++ lista 22/questão 2/ocorrência da chave.c
@@ -1,71 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
- 
-const int lin = 3; 
-const int col = 2; 
- 
+
+const int lin = 2;
+const int col = 2;
+
 void apresentacao(){
-    printf("\nManeira mais eficiete \n\n");
+    printf("\nContador de Chave \n\n");
 }
 
-void lerM(float m[][col]) 
+void lerM(int m[][col]) 
 { 
     for(int i = 0; i < lin; i++){ 
        for(int j = 0; j < col; j++){ 
-           printf("m[%d][%d: ", i + 1, j + 1) ; 
-           scanf("%f", &m[i][j]); 
+           printf("m[%d][%d]: ", i + 1, j + 1) ; 
+           scanf("%d", &m[i][j]); 
+       } 
+    }
+}
+
+void printM(int m[][col]) 
+{ 
+    for(int i = 0; i< lin; i++){ 
+       for(int j = 0; j < col; j++){ 
+           printf("\nm[%d][%d]= %d", i + 1, j + 1, m[i][j]) ; 
        } 
     } 
 } 
 
-void printM(float m[][col]) 
-{ 
-    for(int i = 0; i< lin; i++){ 
-       for(int j = 0; j < col; j++){ 
-           printf("\nm[%d][%d]= %f", i + 1, j + 1, m[i][j]) ; 
-       } 
-    } 
-} 
- 
-bool iguais(float a[][col], float b[][col]) 
+bool iguais(int a[][col], int b[][col]) 
 { 
     bool iguais = true;  
-    int i = 0; 
-    for(int i = 0; i < lin; i++) 
-    { 
-        for(int j = 0; j < col; j++) 
-        { 
+
+    for(int i = 0; i < lin; i++){ 
+        for(int j = 0; j < col; j++){ 
             if(a[i][j] != b[i][j]) iguais = false; 
         } 
     } 
     return iguais; 
 } 
+
+void qtdChave(int m[][col], int chave){
+    int qtd = 0;
+    for (int i = 0; i < lin; i++){
+        for(int j = 0; j < col; j++){
+            if(m[i][j] == chave){
+                qtd++;
+            }
+        }
+    }
+    printf("\nA chave %d aparece %d vez(es) na matriz.", chave, qtd);
+}
  
 int main() { 
- 
  apresentacao();
  
- float m1[lin][col]; 
- 
+ int m1[lin][col]; 
  lerM(m1);  
  
- float m2[lin][col]; 
- 
+ int m2[lin][col]; 
  lerM(m2);  
  
  printf("\n\nmatriz 1\n"); 
- 
  printM(m1);  
  
  printf("\n\nmatriz 2\n"); 
- 
  printM(m2);  
  
- if( iguais(m1, m2) ) printf("\niguais!"); 
- 
- else printf("\ndiferentes!");  
+ if( iguais(m1, m2) ) 
+    printf("\n\nMatrizes iguais!"); 
+ else 
+    printf("\n\nMatrizes diferentes!");  
+
+ int chave;
+ printf("\n\nDigite a chave para contar na matriz 1: ");
+ scanf("%d", &chave);
+ qtdChave(m1, chave);
  
  return 0;
-
-} 
+}
