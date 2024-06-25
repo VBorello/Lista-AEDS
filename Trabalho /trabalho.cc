ca698478409bb977a1d61935a78159de968667ba
Vicenzo <marcosvicenzo8@gmail.com>
2024-06-25 11:01:25 -0300

    more exercices made

--- Trabalho /trabalho.cc
+++ Trabalho /trabalho.cc
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <string>
 
-#define MAX 1000
+#define max 1000
 
-class Data 
-{
+class Data{
+
     private: 
+
         int dia;
         int mes;
         int ano;
+
     public:
-        Data() { }
-        Data(int dia, int mes, int ano)
-        {
+
+        Data() {}
+        Data(int dia, int mes, int ano){
             this->dia = dia;
             this->mes = mes;
             this->ano = ano;
@@ -29,23 +31,19 @@ class Data
 };
 
 
-void Data::setDia(int dia)
-{
+void Data::setDia(int dia){
     this->dia = dia;
 }
 
-void Data::setMes(int mes)
-{
+void Data::setMes(int mes){
     this->mes = mes;
 }
 
-void Data::setAno(int ano)
-{
+void Data::setAno(int ano){
     this->ano = ano;
 }
 
-bool Data::dataValida()
-{
+bool Data::dataValida(){
     bool valida = true;
     if(this->dia < 1 || this->dia > 31 || this->mes < 1 || this->mes > 12 || this->ano < 0)
     {
@@ -54,54 +52,53 @@ bool Data::dataValida()
     return valida;
 }
 
-int Data::getDia()
-{
+int Data::getDia(){
     return this->dia;
 }
 
-int Data::getMes()
-{
+int Data::getMes(){
     return this->mes;
 }
 
-int Data::getAno()
-{
+int Data::getAno(){
     return this->ano;
 }
 
-void Data::leData()
-{
+void Data::leData(){
+
     int dia, mes, ano;
-    do {
+    do{
         std::cout<<"Digite o dia: ";
         std::cin>>dia;
         std::cout<<"Digite o mes: ";
         std::cin>>mes;
         std::cout<<"Digite o ano: ";
         std::cin>>ano;
-    } while(!this->dataValida());
+
+    }while(!this->dataValida());
     setDia(dia);
     setMes(mes);
     setAno(ano);
 }
 
-void Data::escreveData()
-{
+void Data::escreveData(){
     std::cout<<this->dia<<"/"<<this->mes<<"/"<<this->ano<<std::endl;
 }
 
-class Pessoa
-{
+class Pessoa{
+
     private:
+
         std::string nome;
         Data nascimento;
         static int quantidade;
+
     public:
-        static int getQuantidade()
-        {
+
+        static int getQuantidade(){
             return quantidade;
         }
-        Pessoa() {
+        Pessoa(){
             this->nascimento = Data();
             quantidade++;
         }
@@ -116,44 +113,36 @@ class Pessoa
 
 int Pessoa::quantidade = 0;
 
-void Pessoa::setNome(std::string nome)
-{
+void Pessoa::setNome(std::string nome){
     this->nome = nome;
 }
 
-std::string Pessoa::getNome()
-{
+std::string Pessoa::getNome(){
     return this->nome;
 }  
 
-void Pessoa::leNome()
-{
+void Pessoa::leNome(){
     std::string nome;
     std::cout<<"Digite o nome: ";
     std::getline(std::cin, nome);
     setNome(nome);
 }
 
-void Pessoa::escrevaNome()
-{
+void Pessoa::escrevaNome(){
     std::cout<<this->nome<<std::endl;
 }
 
-void Pessoa::lePessoa()
-{
+void Pessoa::lePessoa(){
     leNome();
     nascimento.leData();
-
 }
 
-void Pessoa::escrevaPessoa()
-{
+void Pessoa::escrevaPessoa(){
     escrevaNome();
     nascimento.escreveData();
 }
 
-void cadastro(Pessoa* P[])
-{
+void cadastro(Pessoa* P[]){
     if (Pessoa::getQuantidade() < MAX){
         P[Pessoa::getQuantidade()] = new Pessoa();
         P[Pessoa::getQuantidade()-1]->lePessoa();
@@ -162,8 +151,7 @@ void cadastro(Pessoa* P[])
 
 
 
-int main()
-{
+int main(){
     Pessoa pessoas;
     pessoas.lePessoa();
     pessoas.escrevaPessoa();
