5af73c73091617702d4711d588f9150c1d5c84cb
Vicenzo <marcosvicenzo8@gmail.com>
2024-06-26 09:58:52 -0300

    more exercices made

--- lista 26/poo2.cc
+++ lista 26/poo2.cc
@@ -9,12 +9,50 @@ public:
 
     void setLado(float l){
         if(l <= 0){
-            std::cout << valor negativo é invalido bb>>
+            std::cout << "valor negativo é invalido bb" << std::endl;
+            lado = 0;
         }
         else{
-            
+            lado = l;
         }
     }
 
+    float getLado(){
+        return lado;
+    }
+
+    float area(){
+        return lado * lado;
+    }
+
+    float perimetro(){
+        return lado * 4;
+    }
+
+    void lerLado(){
+        float l;
+        std::cout << "Digite o Lado do Quadrado:";
+        std::cin >> l;
+        setLado(l);
+    }
+
+    void printLado(){
+        std::cout << "\nlado:" << lado << std::endl;
+    }
+
 };
+
+int main(){
+
+    quadrado q;
+
+    q.lerLado();
+
+    q.printLado();
+
+    std::cout << "\nperimetro:" << q.perimetro() << std::endl;
+    std::cout << "\narea:" << q.area() << std::endl;
+
+    return 0;
+}
 
